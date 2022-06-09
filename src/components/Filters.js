365cbe5d5cff39289b32a76c199ad307ee08fe84
devpbdias <devpbdias@gmail.com>
2022-06-09 19:38:05 -0300

    5th requisito refatorado

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,17 +1,49 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import StarWarsContext from '../context/Context';
 
+const OPTIONS_COLUMN = ['population',
+  'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+const OPTIONS_OPERATOR = ['maior que', 'menor que', 'igual a'];
+const COLUMN_ID = 'column-filter';
+
 function Filters() {
-  const { setPlanetsFilter, planetsFilter,
+  const { setPlanetsFilter, planetsFilter, data,
     filterByNumericValues, setfilterByNumericValues } = useContext(StarWarsContext);
-  // const [savePlanetFilter, setSavePlanetFilter] = useState([]);
-  const [msgFilter, setMsgFilter] = useState([]);
 
   const handleNumeric = ({ target }) => {
     setfilterByNumericValues({
       ...filterByNumericValues,
       [target.name]: target.value,
     });
+  };
+
+  const removeColumn = () => {
+    console.log(filterByNumericValues);
+    const columnFilter = filterByNumericValues.column;
+    if (columnFilter === 'population') {
+      const select = document.getElementById(COLUMN_ID);
+      select.remove(select.querySelector('option[value="population"]'));
+    }
+    if (columnFilter === 'orbital_period') {
+      const select = document.getElementById(COLUMN_ID);
+      select.remove(select.querySelector('option[value="orbital_period"]'));
+    }
+    if (columnFilter === 'diameter') {
+      const select = document.getElementById(COLUMN_ID);
+      select.remove(select.querySelector('option[value="diameter"]'));
+    }
+    if (columnFilter === 'rotation_period') {
+      const select = document.getElementById(COLUMN_ID);
+      select.remove(select.querySelector('option[value="rotation_period"]'));
+    }
+    if (columnFilter === 'surface_water') {
+      const select = document.getElementById(COLUMN_ID);
+      select.remove(select.querySelector('option[value="surface_water"]'));
+    }
+  };
+
+  const resetFilter = () => {
+    setPlanetsFilter(data);
   };
 
   const handleClick = () => {
@@ -23,26 +55,21 @@ function Filters() {
       const newPlanetsFilter = planetsFilter
         .filter((values) => Number(values[columnFilter]) > Number([valueFilter]));
       setPlanetsFilter(newPlanetsFilter);
-      setMsgFilter({ ...msgFilter, filterByNumericValues });
     }
 
     if (comparisonFilter === 'menor que') {
       const newPlanetsFilter = planetsFilter
         .filter((values) => Number(values[columnFilter]) < Number([valueFilter]));
       setPlanetsFilter(newPlanetsFilter);
-      setMsgFilter({ ...msgFilter, filterByNumericValues });
     }
 
     if (comparisonFilter === 'igual a') {
       const newPlanetsFilter = planetsFilter
         .filter((values) => Number(values[columnFilter]) === Number([valueFilter]));
       setPlanetsFilter(newPlanetsFilter);
-      setMsgFilter({ ...msgFilter, filterByNumericValues });
     }
-    console.log(msgFilter);
+    removeColumn();
   };
-
-  // toda vez que clickar fazer um filtro do antigo planetsFilter e acrescentar os valores do filtro no <p>
 
   return (
     <div>
@@ -54,11 +81,10 @@ function Filters() {
           id="column-filter"
           data-testid="column-filter"
         >
-          <option defaultValue value="population">population</option>
-          <option value="orbital_period">orbital_period</option>
-          <option value="diameter">diameter</option>
-          <option value="rotation_period">rotation_period</option>
-          <option value="surface_water">surface_water</option>
+          {
+            OPTIONS_COLUMN.map((column, index) => (
+              <option key={ index } value={ column }>{column}</option>))
+          }
         </select>
       </label>
       <label htmlFor="comparison-filter">
@@ -69,9 +95,10 @@ function Filters() {
           id="comparison-filter"
           data-testid="comparison-filter"
         >
-          <option defaultValue value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
+          {
+            OPTIONS_OPERATOR.map((operator, index) => (
+              <option key={ index } value={ operator }>{operator}</option>))
+          }
         </select>
       </label>
       <label htmlFor="button-filter">
@@ -93,7 +120,12 @@ function Filters() {
       >
         Filtrar
       </button>
-      {/* <p>{msgFilter}</p> */}
+      <button
+        type="button"
+        onClick={ resetFilter }
+      >
+        Resetar filtro
+      </button>
     </div>
   );
 }
